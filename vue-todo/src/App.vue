1a06a8fcde4519da6d2e78f18e97636975a24ba8
aamoos <aamoos@naver.com>
2024-02-02 10:50:56 +0900

    할일관리앱 추가기능 - 조회 기능

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,24 +1,57 @@
 <template>
   <div>
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
     <h1>Vue Todo with Typescript</h1>
-    <TodoInput :item="todoText" @input="updateTodoText"></TodoInput>
+    <TodoInput
+      :item="todoText"
+      @input="updateTodoText"
+      @add="addTodoitem"
+    ></TodoInput>
+    <div>
+      <ul>
+        <TodoListItem></TodoListItem>
+        <!-- <li>아이템 1</li>
+        <li>아이템 2</li>
+        <li>아이템 3</li> -->
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || [];
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
     };
   },
   methods: {
-    updateTodoText(value: any) {
+    updateTodoText(value: string) {
       this.todoText = value;
+    },
+    addTodoitem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
     },
   },
 });
